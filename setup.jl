fa7564ed9213952030fa21f8abf1e6b142147a8d
Anirudh <anirudh@semiotic.ai>
2022-10-31 13:43:35 -0700

    Blue style formatting

--- setup.jl
+++ setup.jl
@@ -1,7 +1,7 @@
 import Git as G
 
 using PkgTemplates
-import PkgTemplates
+using PkgTemplates: PkgTemplates
 
 using TOML
 
@@ -18,19 +18,23 @@ function PkgTemplates.badges(::SemanticReleaseBadge)
     )
 end
 
-PkgTemplates.badges(::Documenter{<:PkgTemplates.GitHubPagesStyle}) = [
-    PkgTemplates.Badge(
-        "Latest",
-        "https://img.shields.io/badge/docs-latest-purple.svg",
-        "https://{{{USER}}}.github.io/{{{PKG}}}.jl/latest/",
-    ),
-]
+function PkgTemplates.badges(::Documenter{<:PkgTemplates.GitHubPagesStyle})
+    return [
+        PkgTemplates.Badge(
+            "Latest",
+            "https://img.shields.io/badge/docs-latest-purple.svg",
+            "https://{{{USER}}}.github.io/{{{PKG}}}.jl/latest/",
+        ),
+    ]
+end
 
-PkgTemplates.badges(::Coveralls) = PkgTemplates.Badge(
-    "Coverage Status",
-    "https://img.shields.io/coveralls/github/{{{USER}}}/{{{PKG}}}.jl/badge.svg?branch={{{BRANCH}}}",
-    "https://coveralls.io/github/{{{USER}}}/{{{PKG}}}.jl?branch={{{BRANCH}}}",
-)
+function PkgTemplates.badges(::Coveralls)
+    return PkgTemplates.Badge(
+        "Coverage Status",
+        "https://img.shields.io/coveralls/github/{{{USER}}}/{{{PKG}}}.jl/badge.svg?branch={{{BRANCH}}}",
+        "https://coveralls.io/github/{{{USER}}}/{{{PKG}}}.jl?branch={{{BRANCH}}}",
+    )
+end
 
 """
     createpackage(path::AbstractString)
@@ -41,13 +45,13 @@ Run using `julia -e 'include("setup.jl"); createpackage(path)'`
 """
 function createpackage(path::AbstractString)
     config = TOML.parsefile(path)
-    createpackage(;
+    return createpackage(;
         name=config["name"],
         user=config["user"],
         authors=config["authors"],
         julia=VersionNumber(config["julia"]...),
         dir=config["dir"],
-        license=config["license"]
+        license=config["license"],
     )
 end
 
@@ -82,17 +86,19 @@ function createpackage(; name, user, authors, julia, dir, license)
             SemanticReleaseBadge(),
             CompatHelper(),
             !TagBot,
-        ])
+        ],
+    )
     t(name)
 
     @info "Copying Relevant Files To Package Repo"
-    namesplit = name |> splitext |> first
-    fulldir = dir |> expanduser |> realpath
+    namesplit = first(splitext(name))
+    fulldir = realpath(expanduser(dir))
     path = joinpath(fulldir, namesplit)
     cp("Release.yml", joinpath(path, ".github", "workflows", "Release.yml"))
     cp("dco_check.yml", joinpath(path, ".github", "workflows", "dco_check.yml"))
     cp(".releaserc", joinpath(path, ".releaserc"))
     cp("CHANGELOG.md", joinpath(path, "CHANGELOG.md"))
+    cp(".JuliaFormatter.toml", joinpath(path, ".JuliaFormatter.toml"))
 
     @info "Correcting initial version"
     tomlpath = joinpath(path, "Project.toml")
@@ -101,16 +107,32 @@ function createpackage(; name, user, authors, julia, dir, license)
     @info "Setting correct devurl for Documentation"
     makedocpath = joinpath(path, "docs", "make.jl")
     # \t is 8 spaces, so just use 4 spaces manually
-    modfile(makedocpath, x -> contains(x, "devbranch"), "    devbranch=\"main\",\n    devurl=\"latest\",\n")
+    modfile(
+        makedocpath,
+        x -> contains(x, "devbranch"),
+        "    devbranch=\"main\",\n    devurl=\"latest\",\n",
+    )
 
     @info "Setting git repo for semantic release"
     releasercpath = joinpath(path, ".releaserc")
-    modfile(releasercpath, x -> contains(x, "repositoryUrl"), "  \"repositoryUrl\": \"git@github.com:$user/$name.git\"")
+    modfile(
+        releasercpath,
+        x -> contains(x, "repositoryUrl"),
+        "  \"repositoryUrl\": \"git@github.com:$user/$name.git\"",
+    )
 
     @info "Setting up coveralls"
     ciymlpath = joinpath(path, ".github", "workflows", "CI.yml")
-    modfile(ciymlpath, x -> contains(x, "julia-runtest"), "      - uses: julia-actions/julia-runtest@v1\n      - uses: julia-actions/julia-processcoverage@v1\n")
-    modfile(ciymlpath, x -> contains(x, "COVERALLS_TOKEN"), "          COVERALLS_TOKEN: \${{ secrets.GITHUB_TOKEN }}\n")
+    modfile(
+        ciymlpath,
+        x -> contains(x, "julia-runtest"),
+        "      - uses: julia-actions/julia-runtest@v1\n      - uses: julia-actions/julia-processcoverage@v1\n",
+    )
+    modfile(
+        ciymlpath,
+        x -> contains(x, "COVERALLS_TOKEN"),
+        "          COVERALLS_TOKEN: \${{ secrets.GITHUB_TOKEN }}\n",
+    )
 
     @info "Appending post-setup instructions to README"
     open(joinpath(path, "README.md"), "a") do io
@@ -119,7 +141,7 @@ function createpackage(; name, user, authors, julia, dir, license)
             "\n I generally recommend doing step 1 running this script, but it's not a big deal if you do it after.\n",
             "\n1. Go to [Coveralls](https://coveralls.io/) and activate this repo.",
             "\n2. Once CI finishes running for the first time, go to Settings, Pages, and select the branch `gh-pages`",
-            "\n3. Once you've done these things, delete this TODO section and commit the change with the message `chore: finished repo setup`"
+            "\n3. Once you've done these things, delete this TODO section and commit the change with the message `chore: finished repo setup`",
         ]
         for line in lines
             write(io, line)
@@ -133,7 +155,7 @@ function createpackage(; name, user, authors, julia, dir, license)
     run(`$(G.git()) add .`)
     run(`$(G.git()) commit -m "chore: Set up project"`)
     run(`$(G.git()) tag v0.0.0 -m "Set up project"`)
-    run(`$(G.git()) push --set-upstream origin main v0.0.0`)
+    return run(`$(G.git()) push --set-upstream origin main v0.0.0`)
 end
 
 """
@@ -152,6 +174,6 @@ function modfile(path::String, predicate::F, replacement::String) where {F<:Func
         end
     end
     close(tmpio)
-    mv(tmppath, path, force=true)
+    mv(tmppath, path; force=true)
     return nothing
 end
